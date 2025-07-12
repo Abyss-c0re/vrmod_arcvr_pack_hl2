62b978e885d1d1199fc8bfbbe46d75940012be26
catoni0 <mr.vladimir1337@gmail.com>
2025-07-12 15:08:42 +0300

    integrated with a new melee system

--- lua/weapons/arcticvr_hl2_crowbar/shared.lua
+++ lua/weapons/arcticvr_hl2_crowbar/shared.lua
@@ -1,84 +1,104 @@
 AddCSLuaFile()
-
 SWEP.Spawnable = true
 SWEP.Category = "Half-Life 2 VR"
 SWEP.AdminOnly = false
 SWEP.UseHands = false
-
-SWEP.Base = "arcticvr_base"
-
 SWEP.ViewModel = "models/weapons/arcticvr/melee_crowbar.mdl"
 SWEP.WorldModel = "models/weapons/w_crowbar.mdl"
-
-SWEP.ArcticVR = true
-
 SWEP.Primary.ClipSize = -1
 SWEP.Primary.DefaultClip = 0
 SWEP.Primary.Automatic = false
 SWEP.Primary.Ammo = ""
-
 SWEP.Secondary.ClipSize = -1
 SWEP.Secondary.DefaultClip = -1
 SWEP.Secondary.Automatic = false
 SWEP.Secondary.Ammo = "none"
-
 SWEP.Weight = 50
 SWEP.AutoSwitchTo = false
 SWEP.AutoSwitchFrom = false
-
-SWEP.PrintName = "VR CROWBAR (LEGACY)"
+SWEP.PrintName = "VR CROWBAR"
 SWEP.Slot = 0
 SWEP.SlotPos = 1
 SWEP.DrawAmmo = false
 SWEP.DrawCrosshair = false
-
-SWEP.MeleeAttack = true -- whether this weapon can melee attack
-SWEP.MeleeVelThreshold = 2 -- how fast you have to swing
-SWEP.MeleeDelay = 0.1
-SWEP.MeleeDamage = 35
-SWEP.MeleeDamageType = DMG_CLUB
-
 SWEP.MeleeHitSound = "Weapon_Crowbar.Melee_Hit"
 SWEP.MeleeStrikeSound = "Weapon_Crowbar.Melee_Hit"
-
 SWEP.NotAGun = true
-
 if CLIENT then
+    g_VR.viewModelInfo = g_VR.viewModelInfo or {}
+    g_VR.viewModelInfo.arcticvr_hl2_crowbar = {
+        offsetPos = Vector(3.35, 1.5, 2.5), -- forward, left, up
+        offsetAng = Angle(0, -90, 0),
+        noLaser = true
+    }
 
-g_VR.viewModelInfo = g_VR.viewModelInfo or {}
+    SWEP.FingerAngles = {
+        -- right hand open angles
+        Angle(-25, -5, -20.1), -- finger 0
+        Angle(0, 0, 0),
+        Angle(0, 0, 0),
+        Angle(3, -33, 0), -- finger 1 (index finger, off trigger)
+        Angle(0, -50, 0),
+        Angle(0, -47.9, 0),
+        Angle(0, -23.6, 0), -- finger 2
+        Angle(0, -60, 0),
+        Angle(0, -27, 0),
+        Angle(0, -25.8, 0), -- finger 3
+        Angle(0, -40.6, 0),
+        Angle(0, -45.1, 0),
+        Angle(0, -25.3, -8.2), -- finger 4
+        Angle(0, -34.4, 0),
+        Angle(0, -22.5, 0),
+        -- right hand closed angles
+        Angle(-25, -5, -20.1), -- finger 0
+        Angle(0, 0, 0),
+        Angle(0, 0, 0),
+        Angle(3, -33, 0), -- finger 1 (index finger, on trigger)
+        Angle(0, -50, 0),
+        Angle(0, -47.9, 0),
+        Angle(0, -23.6, 0), -- finger 2
+        Angle(0, -60, 0),
+        Angle(0, -27, 0),
+        Angle(0, -25.8, 0), -- finger 3
+        Angle(0, -40.6, 0),
+        Angle(0, -45.1, 0),
+        Angle(0, -25.3, -8.2), -- finger 4
+        Angle(0, -34.4, 0),
+        Angle(0, -22.5, 0),
+    }
 
-g_VR.viewModelInfo.arcticvr_hl2_crowbar = {
-    offsetPos = Vector(3.35, 1.5, 2.5), --forward, left, up
-    offsetAng = Angle(0, -90, 0),
-}
+    SWEP.BoneIndices = {
+        blender_implicit = 0,
+        knife = 1,
+        bladeend = 2,
+        bladestart = 3,
+    }
 
-SWEP.FingerAngles = {
-    --right hand open angles
-    Angle(-25,-5,-20.1), Angle(0,0,0), Angle(0,0,0), --finger 0
-    Angle(3,-33,0), Angle(0,-50,0), Angle(0,-47.9,0), --finger 1   This is the index finger, off the trigger
-    Angle(0,-23.6,0), Angle(0,-60,0), Angle(0,-27,0), --finger 2
-    Angle(0,-25.8,0), Angle(0,-40.6,0), Angle(0,-45.1,0), --finger 3
-    Angle(0,-25.3,-8.2), Angle(0,-34.4,0), Angle(0,-22.5,0), --finger 4
-    --right hand closed angles
-    Angle(-25,-5,-20.1), Angle(0,0,0), Angle(0,0,0), --finger 0
-    Angle(3,-33,0), Angle(0,-50,0), Angle(0,-47.9,0), --finger 1   This is the index finger, on the trigger
-    Angle(0,-23.6,0), Angle(0,-60,0), Angle(0,-27,0), --finger 2
-    Angle(0,-25.8,0), Angle(0,-40.6,0), Angle(0,-45.1,0), --finger 3
-    Angle(0,-25.3,-8.2), Angle(0,-34.4,0), Angle(0,-22.5,0), --finger 4
-}
+    SWEP.Firemodes = {1, 0}
+    SWEP.Firemode = 1
+end
 
-SWEP.BoneIndices = {
-    blender_implicit = 0,
-    knife = 1,
-    bladeend = 2,
-    bladestart = 3,
-}
+-- Initialize the weapon and add the VRMod_MeleeHit hook
+function SWEP:Initialize()
+    if self.BaseClass.Initialize then self.BaseClass.Initialize(self) end
+    -- Add hook to customize crowbar hits when equipped
+    hook.Add("VRMod_MeleeHit", "ArcticVR_Crowbar_Hit_" .. tostring(self:EntIndex()), function(hitData, callback)
+        local ply = hitData.Attacker
+        if IsValid(ply) and IsValid(ply:GetActiveWeapon()) and ply:GetActiveWeapon() == self then
+            callback("sound/weapons/crowbar/crowbar_impact1.wav", -- Use crowbar hit sound
+                "Impact.Metal", -- Custom decal for crowbar
+                nil, -- Keep default damage
+                nil, -- Keep default multiplier (1.25 for blunt)
+                nil, -- Keep default damage type
+                hitData.Reach * 1.2, -- Slightly increase reach for crowbar
+                hitData.Radius, -- Keep default radius
+                "blunt")
+            -- Set impact type to blunt
+        end
+    end)
+end
 
-SWEP.Firemodes = {
-    1,
-    0
-}
-
-SWEP.Firemode = 1
-
+-- Clean up the hook when the weapon is removed or dropped
+function SWEP:OnRemove()
+    hook.Remove("VRMod_MeleeHit", "ArcticVR_Crowbar_Hit_" .. tostring(self:EntIndex()))
 end
--- lua/weapons/arcticvr_hl2_stunstick/shared.lua
+++ lua/weapons/arcticvr_hl2_stunstick/shared.lua
@@ -1,48 +1,33 @@
 AddCSLuaFile()
-
 SWEP.Spawnable = true
 SWEP.Category = "Half-Life 2 VR"
 SWEP.AdminOnly = false
 SWEP.UseHands = false
-SWEP.Base = "arcticvr_base"
 SWEP.ViewModel = "models/weapons/w_stunbaton.mdl"
 SWEP.WorldModel = "models/weapons/w_stunbaton.mdl"
-SWEP.ArcticVR = true
-
 SWEP.Primary.ClipSize = -1
 SWEP.Primary.DefaultClip = 0
 SWEP.Primary.Automatic = false
 SWEP.Primary.Ammo = ""
-
 SWEP.Secondary.ClipSize = -1
 SWEP.Secondary.DefaultClip = -1
 SWEP.Secondary.Automatic = false
 SWEP.Secondary.Ammo = "none"
-
 SWEP.Weight = 50
 SWEP.AutoSwitchTo = false
 SWEP.AutoSwitchFrom = false
-
 SWEP.PrintName = "VR STUNSTICK"
 SWEP.Slot = 0
 SWEP.SlotPos = 2
 SWEP.DrawAmmo = false
 SWEP.DrawCrosshair = false
-
-SWEP.MeleeAttack = true
-SWEP.MeleeVelThreshold = 2
-SWEP.MeleeDelay = 0.1
-SWEP.MeleeDamage = 40
-SWEP.MeleeDamageType = DMG_CLUB
-SWEP.MeleeHitSound = "Weapon_StunStick.Melee_Hit"
-SWEP.MeleeStrikeSound = "Weapon_StunStick.Activate"
 SWEP.NotAGun = true
-
 if CLIENT then
     g_VR.viewModelInfo = g_VR.viewModelInfo or {}
     g_VR.viewModelInfo.arcticvr_hl2_stunstick = {
         offsetPos = Vector(0, 0, -3), -- forward, left, up
-        offsetAng = Angle(90, 0, 0), -- pitch, yaw, roll (upright grip)
+        offsetAng = Angle(90, 0, 0),
+        noLaser = true -- you don't want a laser pointer 
     }
 
     SWEP.FingerAngles = {
@@ -62,7 +47,6 @@ if CLIENT then
         Angle(0, -25.3, -8.2),
         Angle(0, -34.4, 0),
         Angle(0, -22.5, 0),
-
         -- Right hand closed angles
         Angle(-25, -5, -20.1),
         Angle(0, 0, 0),
@@ -90,4 +74,36 @@ if CLIENT then
 
     SWEP.Firemodes = {1, 0}
     SWEP.Firemode = 1
+end
+
+-- Initialize the weapon and add the VRMod_MeleeHit hook with electric effect
+function SWEP:Initialize()
+    if self.BaseClass.Initialize then self.BaseClass.Initialize(self) end
+    -- Add hook to customize stunstick hits when equipped
+    hook.Add("VRMod_MeleeHit", "ArcticVR_Stunstick_Hit_" .. tostring(self:EntIndex()), function(hitData, callback)
+        local ply = hitData.Attacker
+        if IsValid(ply) and IsValid(ply:GetActiveWeapon()) and ply:GetActiveWeapon() == self then
+            -- Trigger electric spark effect
+            local effectData = EffectData()
+            effectData:SetOrigin(hitData.HitPos)
+            effectData:SetNormal((hitData.HitPos - hitData.Attacker:GetPos()):GetNormalized())
+            effectData:SetMagnitude(2)
+            effectData:SetScale(1)
+            util.Effect("TeslaHitboxes", effectData)
+            callback(nil, -- Use Melee_Hit for entities, Activate for world
+                "Impact.Metal", -- Custom decal for stunstick
+                nil, -- Keep default damage
+                nil, -- Keep default multiplier (1.1 for stunstick)
+                nil, -- Keep default damage type (DMG_CLUB + DMG_SHOCK)
+                hitData.Reach * 1.1, -- Slightly increase reach for stunstick
+                hitData.Radius, -- Keep default radius
+                "stunstick")
+            -- Set impact type to stunstick
+        end
+    end)
+end
+
+-- Clean up the hook when the weapon is removed or dropped
+function SWEP:OnRemove()
+    hook.Remove("VRMod_MeleeHit", "ArcticVR_Stunstick_Hit_" .. tostring(self:EntIndex()))
 end
